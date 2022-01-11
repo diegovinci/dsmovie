502555e88fa10bc738ee0287e125a0de2356b5ba
diegovinci <diego.vinci@outlook.com>
2022-01-10 22:31:19 -0300

    feat: add navbar component

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,23 @@
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
+
+import './styles.css';
+
+function Navbar() {
+  return (
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <h1>DSMovie</h1>
+          <a href="https://github.com/diegovinci">
+            <div className="dsmovie-contact-container">
+              <GithubIcon />
+              <p className="dsmovie-contact-link">/diegovinci</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar;
