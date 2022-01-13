281d57aa07e8abd130e18dc02d3f59f54e5f9d9b
diegovinci <diego.vinci@outlook.com>
2022-01-12 22:49:49 -0300

    feat: add domain model and database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
@@ -0,0 +1,72 @@
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_movie")
+public class Movie {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private Double score;
+	private Integer count;
+	private String image;
+
+	public Movie() {
+	}
+
+	public Movie(Long id, String title, Double score, Integer count, String image) {
+		this.id = id;
+		this.title = title;
+		this.count = count;
+		this.image = image;
+		this.score = score;
+	}
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Integer getCount() {
+		return count;
+	}
+
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
+	}
+
+}
