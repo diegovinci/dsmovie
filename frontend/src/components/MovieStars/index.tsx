0209bb5f3976b8d39c2551548b257e99f468cac7
diegovinci <diego.vinci@outlook.com>
2022-01-14 14:31:34 -0300

    feat: add dynamic data to show movies scores

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -4,14 +4,52 @@ import { ReactComponent as StarEmpty } from 'assets/img/star-empty.svg';
 
 import './styles.css';
 
-function MovieStars() {
+type Props = {
+  score: number;
+}
+
+type StarProps = {
+  fill: number;
+}
+
+// EX:
+// getFills(3.5) => [1, 1, 1, 0.5, 0]
+// getFills(4.1) => [1, 1, 1, 1, 0.5]
+function getFills(score: number) {
+
+  const fills = [0, 0, 0, 0, 0];
+
+  const integerPart = Math.floor(score);
+
+  for (let i = 0; i < integerPart; i++) {
+    fills[i] = 1;
+  }
+
+  const diff = score - integerPart;
+  if (diff > 0) {
+    fills[integerPart] = 0.5;
+  }
+
+  return fills;
+}
+
+function Star({ fill }: StarProps) {
+  switch (fill) {
+    case 0:
+      return <StarEmpty />
+    case 1:
+      return <StarFull />
+    default:
+      return <StarHalf />
+  }
+}
+
+function MovieStars({ score }: Props) {
+  const fills = getFills(score);
+
   return (
     <div className="dsmovie-stars-container">
-      <StarFull />
-      <StarFull />
-      <StarFull />
-      <StarHalf />
-      <StarEmpty />
+      {fills.map(fill => <Star fill={fill} />)}
     </div>
   );
 }
