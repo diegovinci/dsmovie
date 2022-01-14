37e5f364eaf17f764df99b5fe5f839f332a95329
diegovinci <diego.vinci@outlook.com>
2022-01-14 13:03:58 -0300

    feat: add first hooks useState and useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,16 +1,25 @@
 import axios from 'axios';
 import MovieCard from 'components/MovieCard';
 import Pagination from 'components/Pagination';
+import { useEffect, useState } from 'react';
+import { MoviePage } from 'types/movie';
 import { BASE_URL } from 'utils/requests';
 
 function Listing() {
-  axios.get(`${BASE_URL}/movies?page=0`)
-    .then(response => {
-      console.log(response.data);
-    });
+  const [pageNumber, setPageNumber] = useState(0);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=1`)
+      .then(response => {
+        const data = response.data as MoviePage;
+        console.log(data)
+        setPageNumber(data.number);
+      });
+  }, []);
 
   return (
     <>
+      <p>{pageNumber}</p>
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
